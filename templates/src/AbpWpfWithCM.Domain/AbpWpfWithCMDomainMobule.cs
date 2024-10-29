ad38cbc1b6abf302f4f995e70745736832fcbdb7
yitian.chen <chen1tian@hotmail.com>
2024-10-29 21:17:41 +0800

    增加本地化

--- templates/src/AbpWpfWithCM.Domain/AbpWpfWithCMDomainMobule.cs
+++ templates/src/AbpWpfWithCM.Domain/AbpWpfWithCMDomainMobule.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using AbpWpfWithCM.Domain.Localization.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp;
 using Volo.Abp.Domain;
+using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Volo.Abp.MultiTenancy;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.Settings;
 using Volo.Abp.Validation;
+using Volo.Abp.VirtualFileSystem;
 
 namespace AbpWpfWithCM.Domain
 {
@@ -17,10 +20,27 @@ namespace AbpWpfWithCM.Domain
         typeof(AbpDddDomainModule),
         typeof(AbpValidationModule),
         typeof(AbpSettingsModule),
-        typeof(AbpSettingManagementDomainModule)
+        typeof(AbpSettingManagementDomainModule),
+        typeof(AbpLocalizationModule),
+        typeof(AbpVirtualFileSystemModule)
     )]
     public class AbpWpfWithCMDomainMobule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                // "YourRootNameSpace" is the root namespace of your project. It can be empty if your root namespace is empty.
+                options.FileSets.AddEmbedded<AbpWpfWithCMDomainMobule>("AbpWpfWithCM.Domain");
+            });
 
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                //Define a new localization resource (WorkBoxResources)
+                options.Resources
+                    .Add<AbpWpfWithCMResources>("zh-Hans")
+                    .AddVirtualJson("/Localization/Resources/AbpWpfWithCM");
+            });
+        }
     }
 }
--- templates/src/AbpWpfWithCM.WpfApp/ShellViewModel.cs
+++ templates/src/AbpWpfWithCM.WpfApp/ShellViewModel.cs
@@ -1,17 +1,25 @@
-﻿using Caliburn.Micro;
+﻿using AbpWpfWithCM.Domain.Localization.Resources;
+using Caliburn.Micro;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.VirtualFileSystem;
 
 namespace AbpWpfWithCM.WpfApp
 {
     public class ShellViewModel : Conductor<object>
     {
-        public ShellViewModel()
+        private readonly IStringLocalizer<AbpWpfWithCMResources> _localizer;
+
+        public IStringLocalizer<AbpWpfWithCMResources> L => _localizer;
+
+        public ShellViewModel(IStringLocalizer<AbpWpfWithCMResources> localizer)
         {
+            _localizer = localizer;
         }
     }
 }
